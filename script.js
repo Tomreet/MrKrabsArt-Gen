aee44ce81f2beac26772039082f49cf4583eb5a6
Tomreet <kajkovartem20@gmail.com>
2024-09-05 23:24:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -97,6 +97,13 @@ document.addEventListener('DOMContentLoaded', () => {
             promoId: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
             timing: 20000, // 20 seconds
             attempts: 20,
+        },
+        15: {
+            name: 'Stone Age',
+            appToken: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
+            promoId: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
+            timing: 20000, // 40 seconds
+            attempts: 30,
         }
     
     };
