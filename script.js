559a6788871f2c8e2cef213a4bb672f6d21935d6
Tomreet <kajkovartem20@gmail.com>
2024-09-07 02:51:40 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,104 +1,80 @@
 document.addEventListener('DOMContentLoaded', () => {
     const games = {
-        1: {
-            name: 'Riding Extreme 3D',
-            appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
-            promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
-            timing: 30000, // 30 seconds
-            attempts: 25,
-        },
-        2: {
-            name: 'Chain Cube 2048',
-            appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
-            promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
-            timing: 30000, // 30 seconds
-            attempts: 20,
-        },
-        3: {
-            name: 'My Clone Army',
-            appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
-            promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
-            timing: 120000, // 180 seconds
-            attempts: 35,
-        },
-        4: {
-            name: 'Train Miner',
-            appToken: '82647f43-3f87-402d-88dd-09a90025313f',
-            promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
-            timing: 30000, // 30 seconds
-            attempts: 15,
-        },
-        5: {
-            name: 'Merge Away',
-            appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
-            promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
-            timing: 30000, // 30 seconds
-            attempts: 25,
-        },
-        6: {
-            name: 'Twerk Race 3D',
-            appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-            promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-            timing: 12000, // 12 seconds           //4,1 мин
-            attempts: 35,
-        },
-        7 : {
+
+        10 : {
             name: 'Polysphere',
             appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
             promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
             timing: 3000, // 3 seconds               //55 секунд
             attempts: 35,
         },
-        8: {
-            name: 'Mow and Trim',
-            appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
-            promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
-            timing: 20000, // 20 seconds
-            attempts: 20,
-        },
+
         9: {
-            name: 'Mud Racing',
-            appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
-            promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
-            timing: 20000, // 20 seconds
-            attempts: 20,
+            name: 'Twerk Race 3D',
+            appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+            promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+            timing: 12000, // 12 seconds           //4,1 мин
+            attempts: 35,
         },
-        10: {
-            name: 'Cafe Dash',
-            appToken: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-            promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-            timing: 20000, // 20 seconds
-            attempts: 20,
-        },
-        11: {
+
+
+        8: {
             name: 'Zoopolis',
             appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
             promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
             timing: 20000, // 20 seconds
             attempts: 20,
         },
-        12: {
-            name: 'Gangs Wars',
-            appToken: 'b6de60a0-e030-48bb-a551-548372493523',
-            promoId: 'c7821fa7-6632-482c-9635-2bd5798585f9',
-            timing: 40000, // 40 seconds
-            attempts: 35,
+
+        7: {
+            name: 'Merge Away',
+            appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
+            promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
+            timing: 30000, // 30 seconds
+            attempts: 25,
         },
-        13: {
+
+        6: {
+            name: 'Chain Cube 2048',
+            appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
+            promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
+            timing: 30000, // 30 seconds
+            attempts: 20,
+        },
+
+        5: {
+            name: 'Train Miner',
+            appToken: '82647f43-3f87-402d-88dd-09a90025313f',
+            promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
+            timing: 30000, // 30 seconds
+            attempts: 15,
+        },
+
+        4: {
+            name: 'Mow and Trim',
+            appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+            promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+            timing: 20000, // 20 seconds
+            attempts: 20,
+        },
+
+        3: {
             name: 'Fluff Crusade',
             appToken: '112887b0-a8af-4eb2-ac63-d82df78283d9',
             promoId: '112887b0-a8af-4eb2-ac63-d82df78283d9',
             timing: 20000, // 40 seconds
             attempts: 30,
         },
-        14: {
+
+        2: {
             name: 'Tile Trio',
             appToken: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
             promoId: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
             timing: 20000, // 20 seconds
             attempts: 20,
         },
-        15: {
+
+        1: {
             name: 'Stone Age',
             appToken: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
             promoId: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
@@ -141,6 +117,117 @@ document.addEventListener('DOMContentLoaded', () => {
     keyRange.addEventListener('input', () => {
         keyValue.innerText = keyRange.value;
     });
+    // Множитель ключей------------------------------------------------------
+    document.getElementById('generateAllKeysBtn').addEventListener('click', async () => {
+        const gameIds = Object.keys(games).map(Number);
+        const keyCount = {
+            "1": 8, // Stone Age
+            "2": 4, // Tile Trio
+            "3": 4, // Fluff Crusade
+            "4": 4, // Mow and Trim
+            "5": 4, // Train Miner
+            "6": 4, // Chain Cube 2048
+            "7": 4, // Merge Away
+            "8": 4, // Zoopolis
+            "9": 4, // Twerk Race 3D
+            "10": 4 // Polysphere
+        };
+    
+        // Скрываем форму и показываем прогресс
+        document.querySelector('.grid-container').style.display = 'none';
+        keyCountGroup.style.display = 'none';
+        progressContainer.classList.remove('hidden');
+        keyContainer.classList.add('hidden');
+        generatedKeysTitle.classList.add('hidden');
+        keysList.innerHTML = '';
+        copyAllBtn.classList.add('hidden');
+        generateAllKeysBtn.classList.add('hidden');
+    
+        let progress = 0;
+        const updateProgress = (increment, message) => {
+            progress += increment;
+            progress = Math.min(progress, 100); // гарантирует, что прогресс не превысит 100%
+            progressBar.style.width = `${progress}%`;
+            progressText.innerText = `${Math.round(progress)}%`;
+            progressLog.innerText = message;
+        };
+    
+        const allKeys = await Promise.all(gameIds.map(async (gameId) => {
+            const game = games[gameId];
+            const keys = await Promise.all(Array.from({ length: keyCount[gameId] }, async () => {
+                const clientId = generateClientId();
+                let clientToken;
+                try {
+                    clientToken = await login(clientId, game.appToken);
+                } catch (error) {
+                    alert(`Не удалось войти в систему(Перезагрузите генератор или поробуйте позже): ${error.message}`);
+                    return null;
+                }
+    
+                for (let i = 0; i < game.attempts; i++) {
+                    const hasCode = await emulateProgress(clientToken, game.promoId);
+                    updateProgress((100 / game.attempts) / (gameIds.length * keyCount[gameId]), `Эмуляция прогресса ${i + 1}/${game.attempts} для игры ${game.name}...`);
+                    if (hasCode) {
+                        break;
+                    }
+                    await sleep(game.timing);
+                }
+    
+                try {
+                    const key = await generateKey(clientToken, game.promoId);
+                    updateProgress(100 / (gameIds.length * keyCount[gameId]), `Генерация ключа для игры ${game.name}...`);
+                    return key;
+                } catch (error) {
+                    alert(`Не удалось сгенерировать ключ для игры ${game.name}: ${error.message}`);
+                    return null;
+                }
+            }));
+            return keys.filter(key => key);
+        }));
+    
+        const allKeysFlat = allKeys.flat().filter(key => key);
+        if (allKeysFlat.length > 1) {
+            keysList.innerHTML = allKeysFlat.map(key =>
+                `<div class="key-item">
+                    <input type="text" value="${key}" readonly>
+                    <button class="copyKeyBtn" data-key="${key}">Скопировать</button>
+                </div>`
+            ).join('');
+            copyAllBtn.classList.remove('hidden');
+        } else if (allKeysFlat.length === 1) {
+            keysList.innerHTML =
+                `<div class="key-item">
+                    <input type="text" value="${allKeysFlat[0]}" readonly>
+                    <button class="copyKeyBtn" data-key="${allKeysFlat[0]}">Скопировать</button>
+                </div>`;
+        }
+    
+        keyContainer.classList.remove('hidden');
+        generatedKeysTitle.classList.remove('hidden');
+    
+        document.querySelectorAll('.copyKeyBtn').forEach(button => {
+            button.addEventListener('click', (event) => {
+                const key = event.target.getAttribute('data-key');
+                copyToClipboard(key);
+            });
+        });
+    
+        copyAllBtn.addEventListener('click', () => {
+            const keysText = allKeysFlat.join('\n');
+            copyToClipboard(keysText);
+        });
+    
+        progressBar.style.width = '100%';
+        progressText.innerText = '100%';
+        progressLog.innerText = 'Конец генерации';
+    
+        document.querySelector('.grid-container').style.display = 'grid';
+        keyCountGroup.classList.remove('hidden');
+        generateAllKeysBtn.classList.remove('hidden');
+    });
+    
+//-----------------------------------------------------------------
+
 
     startBtn.addEventListener('click', async () => {
         const keyCount = parseInt(keyRange.value);
