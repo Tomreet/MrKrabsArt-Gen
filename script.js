9d271fe494877997005c0d5457aa29a4401c7f42
Tomreet <kajkovartem20@gmail.com>
2024-09-18 22:28:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -196,7 +196,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 try {
                     clientToken = await login(clientId, game.appToken);
                 } catch (error) {
-                    alert(`Не удалось войти в систему(Перезагрузите генератор или поробуйте позже): ${error.message}`);
+                    alert(`Ошибка входа: Failed to login. Превышен лимит запросов, смените IP, включив VPN сервис, или ожидайте 10 минут для обнуления лимитов запросов: ${error.message}`);
                     return null;
                 }
     
@@ -307,7 +307,7 @@ document.addEventListener('DOMContentLoaded', () => {
             try {
                 clientToken = await login(clientId, game.appToken);
             } catch (error) {
-                alert(`Ошибка входа: Failed to login. Превышен лимит запросов, смените IP, включив VPN сервис, или ожидайте 10 минут для обнуления лимитов запросов.');
+                alert(`Ошибка входа: Failed to login. Превышен лимит запросов, смените IP, включив VPN сервис, или ожидайте 10 минут для обнуления лимитов запросов: ${error.message}');
                 startBtn.disabled = false;
                 return null;
             }
